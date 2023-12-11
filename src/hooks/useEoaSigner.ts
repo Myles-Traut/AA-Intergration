dc767d941c4df77ce913a5fd0eed52acda9f331c
Myles Traut <myles@linumlabs.com>
2023-12-11 18:09:13 +0200

    buy tokens

--- src/hooks/useEoaSigner.ts
+++ src/hooks/useEoaSigner.ts
@@ -1,21 +1,8 @@
-import { Address, createWalletClient, custom, http } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-// import { mnemonicToAccount } from "viem/accounts";
+import { createWalletClient, custom } from "viem";
 import { goerli } from "viem/chains";
 import { WalletClientSigner } from "@alchemy/aa-core";
 
 export const useEoaSigner = () => {
-    // if you have a mnemonic, viem also exports a mnemonicToAccount function (see above import)
-    
-    // const account = privateKeyToAccount("0x42f282787d0568d28b7408bf8da906919a9e493000a07e8a9ce8f66c50e94e16");
-    // const account = privateKeyToAccount("0x576a70e6865ffca9f610f6d8ff5367aa9a3c4f068b98842aa8fae3883c790362");
-
-    // This client can now be used to do things like `eth_requestAccounts`
-    // const client = createWalletClient({
-    //     account,
-    //     chain: goerli,
-    //     transport: http(),
-    // });
 
     const client = createWalletClient({
         chain: goerli,
@@ -27,12 +14,6 @@ export const useEoaSigner = () => {
         client,
         "json-rpc" //signerType
       );
-
-    // this can now be used as an owner for a Smart Contract Account
-    // const eoaSigner = new WalletClientSigner(
-    //     client,
-    //     "local" // signerType
-    // );
 
     return {signer : eoaSigner}
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
-import { Address, useAccount, useConnect, useDisconnect } from 'wagmi';
+import { useAccount, useConnect, useContractRead, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { useRouter } from 'next/router';
-import { goerli } from 'viem/chains';
 import { getDefaultEntryPointAddress } from "@alchemy/aa-core";
 import { useCallback, useState } from 'react';
 import { AlchemyProvider } from '@alchemy/aa-alchemy';
 import { LightSmartContractAccount, getDefaultLightAccountFactoryAddress } from '@alchemy/aa-accounts';
 import { useEoaSigner } from '@/hooks/useEoaSigner';
-import { Alchemy, Network } from "alchemy-sdk";
+import { Alchemy, BigNumber, Network } from "alchemy-sdk";
 import { chain, alchemyApiKey } from "@/config/client";
 import { getRpcUrl } from "@/config/rpc";
 import PurchaseTokenForm from '@/components/PurchaseTokenForm';
-import { parseEther } from 'viem';
+import { parseEther, encodeFunctionData, Address } from 'viem';
+import { tokenSaleAbi } from '../../abis/TokenPresale';
+import { createPublicClient, http } from 'viem'
+import { goerli } from 'viem/chains';
+
+type Args = {
+  data: bigint | undefined
+  isError: boolean
+  isLoading: boolean
+}
 
 const Home = () => {
   const { isConnected, address } = useAccount();
@@ -19,8 +27,9 @@ const Home = () => {
     connector: new InjectedConnector(),
   });
   const { disconnect } = useDisconnect();
-  const [scaAddress, setScaAddress] = useState<string>();
-  const [ethBalance, setEthBalance] = useState<string>();
+  const [scaAddress, setScaAddress] = useState<Address>();
+  const [ethBalance, setEthBalance] = useState<string>("");
+  const [tokenBal, setTokenBal] = useState<string>("");
   const [currentProvider, setCurrentProvider] = useState<AlchemyProvider>(
     new AlchemyProvider({
     chain,
@@ -28,11 +37,17 @@ const Home = () => {
   }))
   const [amount, setAmount] = useState<string>('');
   const [to, setTo] = useState<string>("");
+  const [tokenAmount, setTokenAmount] = useState<string>('');
 
   const router = useRouter();
   const alchemy = new Alchemy({
     network: Network.ETH_GOERLI,
     apiKey: alchemyApiKey,
+    
+  });
+  const publicClient = createPublicClient({
+    chain: goerli,
+    transport: http()
   });
 
   const login = useCallback(async () => {
@@ -40,6 +55,12 @@ const Home = () => {
     const provider: AlchemyProvider = new AlchemyProvider({
       apiKey: alchemyApiKey,
       chain: goerli,
+      opts: {
+        txMaxRetries: 20,
+        txRetryIntervalMs: 2_000,
+        txRetryMulitplier: 1.5,
+        minPriorityFeePerBid: 100_000_000n,
+      },
     }).connect(
       (rpcClient) =>
         new LightSmartContractAccount({
@@ -54,6 +75,14 @@ const Home = () => {
     setScaAddress(contractAddress);
     setEthBalance((await alchemy.core.getBalance(contractAddress)).toString());
     setCurrentProvider(provider);
+
+    const data = await publicClient.readContract({
+          address: '0xD055B32fd3136F1dCA638Cd8f4B2eAF4A10abAb3',
+          abi: tokenSaleAbi,
+          functionName: 'userHubBalance',
+          args: [contractAddress]
+        })
+    setTokenBal(data.toString());
   },[]);
 
   const sendETH = useCallback(async() => {
@@ -61,6 +90,12 @@ const Home = () => {
     const provider: AlchemyProvider = new AlchemyProvider({
       apiKey: alchemyApiKey,
       chain: goerli,
+      opts: {
+        txMaxRetries: 20,
+        txRetryIntervalMs: 2_000,
+        txRetryMulitplier: 1.5,
+        minPriorityFeePerBid: 100_000_000n,
+      },
     }).connect(
       (rpcClient) =>
         new LightSmartContractAccount({
@@ -82,10 +117,70 @@ const Home = () => {
     console.log("Transaction Hash: ", txHash);
   }, []); 
 
-  console.log(currentProvider);
+  const buyToken = useCallback(async() => {
+    const { signer } = useEoaSigner();
+    const provider: AlchemyProvider = new AlchemyProvider({
+      apiKey: alchemyApiKey,
+      chain: goerli,
+      opts: {
+        txMaxRetries: 20,
+        txRetryIntervalMs: 2_000,
+        txRetryMulitplier: 1.5,
+        minPriorityFeePerBid: 100_000_000n,
+      },
+    }).connect(
+      (rpcClient) =>
+        new LightSmartContractAccount({
+          chain,
+          owner: signer,
+          factoryAddress: getDefaultLightAccountFactoryAddress(chain),
+          rpcClient,
+        })
+    );
+    const uoCallData = encodeFunctionData({
+      abi: tokenSaleAbi,
+      functionName: 'buyHub',
+      args: [await provider.getAddress()],
+    });
+    const uo = await provider.sendUserOperation({
+      target: "0xD055B32fd3136F1dCA638Cd8f4B2eAF4A10abAb3",
+      data: uoCallData,
+      value: parseEther(tokenAmount),
+    });
+    console.log(tokenAmount);
+    console.log(uo.hash);
+    const txHash = await provider.waitForUserOperationTransaction(uo.hash);
+    console.log(txHash);
+  }, []);
+
+  const getTokenBalance = useCallback(async() => {
+    const { signer } = useEoaSigner();
+    const provider: AlchemyProvider = new AlchemyProvider({
+      apiKey: alchemyApiKey,
+      chain: goerli,
+    }).connect(
+      (rpcClient) =>
+        new LightSmartContractAccount({
+          chain,
+          owner: signer,
+          factoryAddress: getDefaultLightAccountFactoryAddress(chain),
+          rpcClient,
+        })
+    );
+    
+    const contractAddr = await provider.getAddress();
+    const data = await publicClient.readContract({
+      address: '0xD055B32fd3136F1dCA638Cd8f4B2eAF4A10abAb3',
+      abi: tokenSaleAbi,
+      functionName: 'userHubBalance',
+      args: [contractAddr]
+    })
+    console.log(data);
+    setTokenBal(data.toString());
+  }, []);
 
   return (
-    <>
+    <div className="ml-4 mt-4">
     <div>Home</div>
     <div className="flex relative items-center" >
       {isConnected ? 
@@ -93,13 +188,10 @@ const Home = () => {
       <button
       className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
       onClick={() => {disconnect(); router.push('/')}}>Disconnect</button>
-      <div>MM Address: {address}</div>
-      <button
-      className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-      onClick={login}>Deploy SCA</button>
-      <div>SCA ADDRESS: {scaAddress}</div>
-      <div>SCA Balance: {ethBalance}</div>
-      {/* <button onClick={sendETH}>Send ETH</button> */}
+      <div>Signer Address: {address}</div>
+      <div>Smart Wallet Address: {scaAddress}</div>
+      <div>Smart Wallet Balance: {ethBalance}</div>
+      <br />
       <form onSubmit={(e) => {
             e.preventDefault();
             sendETH();
@@ -123,24 +215,49 @@ const Home = () => {
                   onChange={(e) => setTo(e.target.value)}
               />
             </div>
-            <div className="items-center pl-28 mt-4">
+            <div className="mt-4">
                 <button 
-                    className={isConnected ? 
-                            "h-10 px-5 m-2 text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800" : 
-                            "h-10 px-5 m-2 text-gray-700 transition-colors duration-150 bg-gray-400 rounded-lg focus:shadow-outline hover:bg-gray-500"}>
-                    Transfer
+                    className= "h-10 px-5 m-2 text-green-100 bg-green-700 rounded-lg hover:bg-green-800">
+                    Transfer ETH
                 </button>
             </div>
+            <hr />
             <div>
             </div>
         </form>
-      </div> :
+        
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          buyToken();
+        }}>
+          <label htmlFor="Buy Token" className="pr-4">Amount to Spend</label>
+            <input
+              value={tokenAmount}
+              className="text-black pl-2"  
+              id="Buy" 
+              placeholder="0.001 ETH"
+              onChange={(e) => setTokenAmount(e.target.value)}
+          />
+          <button 
+              className= "h-10 px-5 m-2 text-green-100 bg-green-700 rounded-lg hover:bg-green-800">
+              Buy Token
+          </button>
+        </form>
+        
+        <div><button className= "h-10 px-5 m-2 text-green-100 bg-green-700 rounded-lg hover:bg-green-800" onClick={getTokenBalance}>Get balance</button>
+        Token Balance: {tokenBal}</div>
+      </div>
+       :
       <button 
       className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-      onClick={() => connect()}>Connect Wallet</button>
+      onClick={() => {
+        connect();
+        login();
+        getTokenBalance();
+      }}>Connect Wallet</button>
       }
     </div>
-  </>)
+  </div>)
 }
 
 export default Home;
