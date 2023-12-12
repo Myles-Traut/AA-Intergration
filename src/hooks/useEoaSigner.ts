d70706b822abf70c52bcafd77cfe8b17151fa8a5
Bevan Christians <gaelicgod@gmail.com>
2023-12-12 18:21:27 +0200

    fix: switch to using useEffect

--- src/hooks/useEoaSigner.ts
+++ src/hooks/useEoaSigner.ts
@@ -1,19 +1,27 @@
 import { createWalletClient, custom } from "viem";
 import { polygon } from "viem/chains";
 import { WalletClientSigner } from "@alchemy/aa-core";
+import { useEffect, useState } from "react";
 
 export const useEoaSigner = () => {
+  const [client, setClient] = useState<any>();
 
-    const client = createWalletClient({
-        chain: polygon,
-        transport: custom(window.ethereum),
-      });
-      
-      // this can now be used as an owner for a Smart Contract Account
-      const eoaSigner = new WalletClientSigner(
-        client,
-        "json-rpc" //signerType
+  useEffect(() => {
+    if (window.ethereum) {
+      setClient(
+        createWalletClient({
+          chain: polygon,
+          transport: custom(window.ethereum),
+        })
       );
+    }
+  }, [setClient]);
 
-    return {signer : eoaSigner}
-}
+  // this can now be used as an owner for a Smart Contract Account
+  const eoaSigner = new WalletClientSigner(
+    client,
+    "json-rpc" //signerType
+  );
+
+  return { signer: eoaSigner };
+};
